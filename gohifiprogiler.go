e44ca0daba227a5b62361df88e201fedaeff22fa
Gaurav Sablok <gauravcodepro@gmail.com>
2024-08-07 11:47:33 +0200

    Create gohifiprogiler.go

---
+++ gohifiprogiler.go
@@ -0,0 +1,11 @@
+#! /usr/bin/go 
+
+/*
+Author Gaurav Sablok
+Universitat Potsdam Library
+Date: 2024-8-7
+
+A pacbiohifi go profiler that takes the pacbiohifi reads, makes the hapmers, filters the hapmers according to the 
+critera and rewrites the pacbiohifi for effecient buble construction while making the graphs for genome assembly. 
+
+*/
