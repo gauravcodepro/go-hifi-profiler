e0203367447135ac2b1475e05795072fbdb4ed94
Gaurav Sablok <gauravcodepro@gmail.com>
2024-08-08 14:32:57 +0200

    2024-8-8 update
    finished code and started building the desktop application

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 - a type interface for the profiling of the pacbiohifi reads, making hapmers and selective filtering of the hapmers for bubble pop
 - this takes a concurrency slice mapping for the filtering of the hapmers so that the reads with the bubble clog can be filtered out.
+- A pacbiohifi go profiler that takes the pacbiohifi reads, makes the mers according to the length, filters the mers according to the critera.
+- This is supported witht a desktop application to see that if your reads have high profiled genomic mers that could hinder the graph assembly.
 
 Gaurav Sablok
--- gohifiprogiler.go
+++ gohifiprogiler.go
@@ -1,11 +1,81 @@
-#! /usr/bin/go 
-
 /*
 Author Gaurav Sablok
 Universitat Potsdam Library
 Date: 2024-8-7
 
-A pacbiohifi go profiler that takes the pacbiohifi reads, makes the hapmers, filters the hapmers according to the 
-critera and rewrites the pacbiohifi for effecient buble construction while making the graphs for genome assembly. 
+A pacbiohifi go profiler that takes the pacbiohifi reads, makes the mers according to the length,
+filters the mers according to the critera. This is supported witht a desktop application to see that
+if your reads have high profiled genomic mers that could hinder the graph assembly.
 
 */
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	readfile, err := os.Open("/home/gauravcodepro/Desktop/coderep/samplefile.fastq")
+	if err != nil {
+		panic(err)
+		log.Fatal(err)
+		return
+	}
+	readbuffer := bufio.NewScanner(readfile)
+	header := []string{}
+	sequences := []string{}
+	header := []string{}
+	sequences := []string{}
+	for readbuffer.Scan() {
+		line := readbuffer.Text()
+		if string(line[0]) == "A" || string(line[0]) == "T" || string(line[0]) == "G" || string(line[0]) == "C" {
+			sequences = append(sequences, line)
+		}
+		if string(line[0]) == "@" {
+			header = append(header, line)
+		}
+	}
+
+	seqtok := []string{}
+	for i := range sequences {
+		for j := 0; j <= len(sequences[i])-2; j++ {
+			seqtok = append(seqtok, string(sequences[i][j:j+2]))
+		}
+	}
+
+	kmercomp := []int{}
+	for i := range seqtok {
+		hold := string(seqtok[i])
+		kmercomp = append(kmercomp, strings.Count(hold, "A")+strings.Count(hold, "T")+strings.Count(hold, "G")+strings.Count(hold, "C"))
+	}
+
+	kmercompGC := []int{}
+	for i := range seqtok {
+		hold := string(seqtok[i])
+		kmercomp = append(kmercomp, strings.Count(hold, "G")+strings.Count(hold, "C"))
+	}
+
+	kmerclassify := []string{}
+	kmerfilter := []int{}
+	for i := range seqtok {
+		kmerclassify = append(kmerclassify, seqtok[i])
+		kmerfilter = append(kmerfilter, int(kmercompGC[i])/int(kmercomp[i]))
+	}
+
+	filteredKmer := []string{}
+	for i := range kmerclassify {
+		if kmerfilter[i] < 10 {
+			filteredKmer = append(filteredKmer, kmerclassify[i])
+		}
+	}
+
+	for i := range filteredKmer {
+		fmt.Println("The filtered kmers with the elective selection are %s and their length are %T:", string(filteredKmer[i]), len(string(filteredKmer[i])))
+	}
+
+}
